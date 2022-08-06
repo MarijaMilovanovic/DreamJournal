5f9a5ca14ba2cccd133c3c789fc8f311ccefb094
MarijaMilovanovic <marijam1337@gmail.com>
2022-08-06 20:27:13 +0200

    logger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express, { json } from 'express';
 import mongoose from 'mongoose';
+import morganMiddleware from './logger';
 import dreamRoutes from './routes/dreams'
 import dreamTypesRoutes from './routes/dreamTypes'
 
 const app = express();
 
 app.use(json())
+app.use(morganMiddleware);
 
 const PORT = process.env.PORT || 3000;
 const dbUrl = "mongodb://localhost:27017/DreamJournal"
